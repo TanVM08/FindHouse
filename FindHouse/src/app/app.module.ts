a73933107ccca99ba64a49621e2b7d03d80a3115
ADMIN4\Admin <vuminhtan0103@gmail.com>
2023-04-25 17:40:06 +0700

    TanVM commit

--- FindHouse/src/app/app.module.ts
+++ FindHouse/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponentComponent } from './views/footer-component/footer-component.component';
+import { HeaderComponentComponent } from './views/header-component/header-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponentComponent,
+    HeaderComponentComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
